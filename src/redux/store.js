a0fca1e5370df5d599edba362f6f355495b57cac
kamran ali <km775.ali@gmail.com>
2024-03-22 21:33:00 +0500

    auth reducer implemented

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-
-const rootReducers = combineReducers({});
+import { authReducer } from "./Auth/Reducer";
+const rootReducers = combineReducers({
+  auth: authReducer,
+});
 const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
