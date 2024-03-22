cfb51221d102437513f9c3e72ac0a01d71a4f3b4
kamran ali <km775.ali@gmail.com>
2024-03-22 20:42:36 +0500

    redux store created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import Navbar from "../components/navigation/Navbar";
 import FooterComponent from "../components/footer/Footer";
+import Layout from "../components/Layout";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <FooterComponent />
+        <Layout>
+          <Navbar />
+          {children}
+          <FooterComponent />
+        </Layout>
       </body>
     </html>
   );
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducers = combineReducers({});
+const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
