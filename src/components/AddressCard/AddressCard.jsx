bc2845d5d65bb975332b64e9e265f9b0a0726644
kamran ali <km775.ali@gmail.com>
2024-03-22 08:27:39 +0500

    address section added  in checkout page

--- src/components/AddressCard/AddressCard.jsx
+++ src/components/AddressCard/AddressCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const AddressCard = () => {
-  return <div>AddressCard</div>;
+  return (
+    <div className="space-y-3">
+      <p className="font-semibold">Kamran Ali</p>
+      <p>Muslim Town Mor Wahdat Road</p>
+      <div className="space-y-1">
+        <p className="font-semibold">Phone Number</p>
+        <p>03095543952</p>
+      </div>
+    </div>
+  );
 };
 
 export default AddressCard;
